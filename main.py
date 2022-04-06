3a4e412eea1c65d845f853a47fae64c1bcb3486e
Tom <tomrunting@googlemail.com>
2022-04-07 00:07:06 +0100

    Test predictions on imgur file

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+import logging
+import predict
 from flask import Flask, render_template
+from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 
+# Load the model
+model_path = 'models/test_model.h5'
+model = load_model(model_path)
+
+# Predict
+prediction = predict.test(model, 28, 28)
+
 @app.route('/')
 def root():
-    return render_template('index.html')
+    return render_template('index.html', pred=prediction)
 
 if __name__ == '__main__':
     # Used only when running locally
     app.run(host='127.0.0.1', port=8080, debug=True)
+
+if __name__ != '__main__':
+    # Add gunicorn logging functionality
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
---
+++ predict.py
@@ -0,0 +1,25 @@
+import imageio
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+# Suppress warnings, don't use gpu
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+# Test on an imgur url
+def test(model, img_rows, img_cols):
+    # Grab image
+    im = imageio.imread("https://i.imgur.com/Wq9CTM1.png")
+
+    # Process as grayscale
+    gray = np.dot(im, [0.2, 0.1, 0.3])  # Define our own RGB to grayscale mapping
+
+    # Reshape and normalise
+    gray = gray.reshape(1, img_rows, img_cols, 1)
+    gray /= 255
+
+    # Predict the digit
+    prediction = model.predict(gray)
+
+    return prediction.argmax()
